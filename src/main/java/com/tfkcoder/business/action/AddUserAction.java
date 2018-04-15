6959890da13ad77eb717a0406c0b28dccd1e2198
youminxiaodao <1126519721@qq.com>
2018-04-15 21:20:54 +0800

    Signed-off-by: youminxiaodao <1126519721@qq.com>

--- src/main/java/com/tfkcoder/business/action/AddUserAction.java
+++ src/main/java/com/tfkcoder/business/action/AddUserAction.java
@@ -4,27 +4,44 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.opensymphony.xwork2.Action;
+import com.tfkcoder.business.entity.User;
 import com.tfkcoder.business.service.UserService;
 
 @Controller
 public class AddUserAction {
 	@Autowired
 	private UserService userService;
-	
+
 	private String username;
-	
+
 	private String password;
-	
+
 	private String message;
 
 	public String execute() {
-		
+
 		userService.addUser(username, password);
-		
+
 		return Action.SUCCESS;
 	}
-		
-		
+	
+	public String login() {
+
+		if (username == null || password == null) {
+			System.out.println("用户名或密码不能为空");
+			return Action.ERROR;
+		}
+
+		User user = userService.login(username, password);
+		if (user == null) {
+			return Action.ERROR;
+		} else {
+			System.out.println(user);
+			System.out.println("登陸成功！！");
+		}
+		return Action.SUCCESS;
+	}
+
 	public String getUsername() {
 		return username;
 	}
